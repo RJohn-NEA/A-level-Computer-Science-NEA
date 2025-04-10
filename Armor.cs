07c0d53db0f166dd5cd5f608bfee88ec51cee2f1
RJohn-NEA <ryangj07@gmail.com>
2025-04-10 12:25:52 +0100

    Armor Data Script

---
+++ Armor.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Armor : MonoBehaviour
+{
+    [Header("Armor Stats")]
+    public float defence;
+
+    [Header("Shop")]
+    public string armorName;
+    public Sprite armorIcon;
+    public float price;
+    public int levelUnlocked; // To see if the player is the right level to be able to buy the weapon
+    public Exp exp;
+    public bool owned; // To check if the player has bought the item
+    public bool equipped; // To check if the player is trying to equip the item
+    public bool unlocked; // Used to check if the player has unlocked the weapon or not
+
+    public GameObject armorModel;
+    
+    private void Update()
+    {
+        // If the player has tried to equip the item
+        if (equipped == true)
+        {
+            Equip();
+        }
+        // If the item is unequipped for the player
+        else if (equipped == false)
+        {
+            Unequip();
+        }
+
+        // If the players level is high enough to unlock the weapon
+        if (exp.currentLevel >= levelUnlocked)
+        {
+            // Used in shop manager to see if they are able to buy the weapon
+            unlocked = true;
+        }
+        else
+        {
+            unlocked = false;
+        }
+    }
+
+    public void Equip()
+    {
+        // Enable the armor model
+        armorModel.SetActive(true);
+    }
+
+    public void Unequip()
+    {
+        // Disable the armor model
+        armorModel.SetActive(false);
+    }
+}
