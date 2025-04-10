5ad6e10e3bf53ba8e27e1a6cfef0aee6f9b735b5
RJohn-NEA <ryangj07@gmail.com>
2025-04-10 13:49:10 +0100

    Ranged Weapon Script

---
+++ RangedWeapon.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class RangedWeapon : MonoBehaviour
+{
+    [Header("Weapon Stats")]
+    public float Damage;
+    public float range;
+    public GameObject rangedWeaponObj;
+
+    [Header("Shop")]
+    public string weaponName;
+    public Sprite weaponIcon;
+    public float price;
+    public int levelUnlocked; // To see if the player is the right level to be able to buy the weapon
+    public Exp exp;
+    public bool owned; // To check if the player has bought the item
+    public bool equipped; // To check if the player is trying to equip the item
+    public bool unlocked; // Used to check if the player has unlocked the weapon or not
+
+    private void Update()
+    {
+        // If the players level is high enough to unlock the weapon
+        if (exp.currentLevel >= levelUnlocked)
+        {
+            // Used in shop manager to see if they are able to buy the weapon
+            unlocked = true;
+        }
+        else
+        {
+            unlocked = false;
+        }
+    }
+}
